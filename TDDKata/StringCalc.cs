4f655d88f0d1badd40b238169000995755cf9ba7
nigrei.av <Nigrei.AV@omsk.gazprom-neft.ru>
2020-12-07 18:29:54 +0600

    second push

--- TDDKata/StringCalc.cs
+++ TDDKata/StringCalc.cs
@@ -1,14 +1,39 @@
 ﻿// NUnit 3 tests
 // See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
-using System;
 
 namespace TDDKata
 {
     internal class StringCalc
     {
-        internal int Sum(string v)
+        internal int Sum(string s)
         {
-            throw new NotImplementedException();
+            if (s != null)
+            {
+
+                if (s.Length == 0)
+                {
+                    return 0;
+                }
+
+                int sum = 0;
+                string[] values = s.Split(',');
+
+                foreach (var value in values)
+                {
+                    if (int.TryParse(value, out var number))
+                    {
+                        sum += number;
+                    }
+                    else
+                    {
+                        return -1;
+                    }
+                }
+
+                return sum;
+            }
+
+            return -1;
         }
     }
 }
