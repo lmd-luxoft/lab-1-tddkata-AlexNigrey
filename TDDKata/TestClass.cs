1c42b7353f89feadd13ef00f01e95d7653c57b79
nigrei.av <Nigrei.AV@omsk.gazprom-neft.ru>
2020-12-07 17:30:03 +0600

    Test commit

--- TDDKata/TestClass.cs
+++ TDDKata/TestClass.cs
@@ -10,11 +10,59 @@ namespace TDDKata
     public class TestClass
     {
         [Test]
-        public void SimpleTest()
+        public void CorrectSumTest()
         {
             StringCalc calc = new StringCalc();
-            int value = calc.Add("2,2");
-            Assert.That(value, Is.EqualTo(4), "Wrong actual value");
+            string op = "1,2";
+            int expected = 3;
+            int  actual = calc.Sum(op);
+            
+            Assert.AreEqual(actual, expected);
         }
+
+        [Test]
+        public void EmptySumTest()
+        {
+            StringCalc calc = new StringCalc();
+            string op = "";
+            int expected = 0;
+            int actual = calc.Sum(op);
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void NullSumTest()
+        {
+            StringCalc calc = new StringCalc();
+            string op = null;
+            int expected = -1;
+            int actual = calc.Sum(op);
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void WrongSumTest()
+        {
+            StringCalc calc = new StringCalc();
+            string op = "1,!";
+            int expected = -1;
+            int actual = calc.Sum(op);
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void NotCompletedSumTest()
+        {
+            StringCalc calc = new StringCalc();
+            string op = ",1";
+            int expected = -1;
+            int actual = calc.Sum(op);
+
+            Assert.AreEqual(actual, expected);
+        }
+
     }
 }
